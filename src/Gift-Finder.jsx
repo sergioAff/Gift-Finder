27edbd8683c834c7396f7b0b78751d4d13372274
Sergio Adrian Fernández <118078644+sergioAff@users.noreply.github.com>
2024-05-21 17:35:09 -0400

    Creacion de los directorios

---
+++ src/Gift-Finder.jsx
@@ -0,0 +1,6 @@
+export const Gift_Finder=()=>{
+    const ApiKey='ERFmClN1aQr20nkBOhATZSn3EFaq5cZW'
+    return(
+        <h1>Hola</h1>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {Gift_Finder} from './Gift-Finder'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Gift_Finder />
+  </React.StrictMode>,
+)
