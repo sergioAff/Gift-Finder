ad546dac04924e72978c4fed321ae8be0ca0dfd9
Sergio Adrian Fernández <118078644+sergioAff@users.noreply.github.com>
2024-05-23 14:37:18 -0400

    Update GifGrid.jsx

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,5 +1,14 @@
 export const GifGrid = ({ category }) => {
 
+  const getGif=async()=>{
+    const url=`https://api.giphy.com/v1/gifs/search?api_key=ERFmClN1aQr20nkBOhATZSn3EFaq5cZW&q=${category}`
+
+    const resp=await fetch(url)
+
+    console.log(resp)
+  }
+
+  getGif()
 
   return (
     <>
