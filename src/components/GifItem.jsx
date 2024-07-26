a0d5e1d7fa5ee016fd97de3af591e91f4e31ad6f
Sergio Fernandez <sergioadrianfernandez02@gmail.com>
2024-07-26 09:07:31 -0400

    add loading

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,16 +1,21 @@
+import { Suspense } from "react";
+import { LoadingSpinner } from "./LoadingSpinner";
+
 export const Gifitem = ({ title, url }) => {
   return (
-    <div className="bg-gray-800 rounded-lg shadow-lg transform transition-transform hover:scale-105">
-      <figure className="flex flex-col items-center p-4">
-        <img
-          src={url}
-          alt={title}
-          className="rounded-md mb-2 w-full h-64 object-cover"
-        />
-        <figcaption className="text-center text-gray-200 mb-2">
-          {title}
-        </figcaption>
-      </figure>
-    </div>
+    <Suspense fallback={<LoadingSpinner />}>
+      <div className="bg-gray-800 rounded-lg shadow-lg transform transition-transform hover:scale-105">
+        <figure className="flex flex-col items-center p-4">
+          <img
+            src={url}
+            alt={title}
+            className="rounded-md mb-2 w-full h-64 object-cover"
+          />
+          <figcaption className="text-center text-gray-200 mb-2">
+            {title}
+          </figcaption>
+        </figure>
+      </div>
+    </Suspense>
   );
 };
